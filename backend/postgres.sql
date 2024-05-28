31b5437a6015c73878da620586cbbaaa1a17eb81
rusyd4 <naufalrs9708@gmail.com>
2024-05-28 21:22:04 +0700

    Drop SQL dump

--- backend/postgres.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE entry_type AS ENUM ('income', 'expense');
-
-CREATE TABLE entries (
-  id SERIAL PRIMARY KEY,
-  description VARCHAR(255) NOT NULL,
-  amount NUMERIC NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  type entry_type NOT NULL
-);
